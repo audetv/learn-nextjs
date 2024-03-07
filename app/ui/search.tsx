b990ea7813cab463d225174090f8bc2edf17812b
audetv <algsound@yandex.ru>
2024-03-07 22:21:47 +0300

    Update the URL with the search params

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,11 +2,23 @@
 
 import {MagnifyingGlassIcon} from '@heroicons/react/24/outline';
 import React from "react";
+import {usePathname, useRouter, useSearchParams} from "next/navigation";
+import * as repl from "repl";
 
 export default function Search({placeholder}: { placeholder: string }) {
 
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const {replace} = useRouter();
     function handleSearch(term: string) {
+        const params = new URLSearchParams(searchParams)
         console.log('Search term:', term);
+        if (term) {
+            params.set('query', term);
+        } else {
+            params.delete('query');
+        }
+        replace(`${pathname}?${params.toString()}`);
     }
 
     return (
