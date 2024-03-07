acd3a887b6c82fa752d7bd5fa78bcb1fba7753f3
audetv <algsound@yandex.ru>
2024-03-07 20:42:32 +0300

    Fetching data for the dashboard overview page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,15 @@
-export default function Page() {
-    return <p>Dashboard Page</p>
+import {lusitana} from "@/app/ui/fonts";
+
+export default async function Page() {
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
+            <div className="grid gap-5 sm:grid-cols-2 lg:grid-cols-4">
+                1
+            </div>
+            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                2
+            </div>
+        </main>
+    );
 }
