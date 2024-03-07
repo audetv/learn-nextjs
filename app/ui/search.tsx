1f879dc6d1923e031134d3804b609820f03ed183
audetv <algsound@yandex.ru>
2024-03-07 23:34:38 +0300

    Best practice: Debouncing

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -4,22 +4,23 @@ import {MagnifyingGlassIcon} from '@heroicons/react/24/outline';
 import React from "react";
 import {usePathname, useRouter, useSearchParams} from "next/navigation";
 import * as repl from "repl";
+import {useDebouncedCallback} from "use-debounce";
 
 export default function Search({placeholder}: { placeholder: string }) {
 
     const searchParams = useSearchParams();
     const pathname = usePathname();
     const {replace} = useRouter();
-    function handleSearch(term: string) {
+    const handleSearch = useDebouncedCallback((term) => {
         const params = new URLSearchParams(searchParams)
-        console.log('Search term:', term);
+        console.log(`Searching… ${term}`);
         if (term) {
             params.set('query', term);
         } else {
             params.delete('query');
         }
         replace(`${pathname}?${params.toString()}`);
-    }
+    }, 500);
 
     return (
         <div className="relative flex flex-1 flex-shrink-0">
