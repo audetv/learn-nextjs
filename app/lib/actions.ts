b5d8db7ce19e7ec1cac0401635150d415d03b063
audetv <algsound@yandex.ru>
2024-03-08 14:44:43 +0300

    Pass the id to the Server Action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -14,6 +14,7 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({id: true, date: true});
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
     const {customerId, amount, status} = CreateInvoice.parse({
@@ -27,7 +28,25 @@ export async function createInvoice(formData: FormData) {
 
     await sql`
         INSERT into invoices(customer_id, amount, status, date)
-        VALUES (${customerId}, ${amount}, ${status}, ${date})
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices')
+}
+
+export async function updateInvoice(id: string, fromData: FormData) {
+    const {customerId, amount, status} = UpdateInvoice.parse({
+            customerId: fromData.get('customerId'),
+            amount: fromData.get('amount'),
+            status: fromData.get('status')
+    });
+
+    const amountCents = amount * 100;
+
+    await sql`UPDATE invoices
+                SET customer_id = ${customerId}, amount = ${amountCents}, status = ${status}
+                WHERE id = ${id}
     `;
 
     revalidatePath('/dashboard/invoices');
