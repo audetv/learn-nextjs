0fcb2985cc8d9f7ef9aaada9e5722fb0b3f32848
audetv <algsound@yandex.ru>
2024-03-08 14:44:29 +0300

    . Fetch the specific invoice

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,9 +1,13 @@
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import Form from "@/app/ui/invoices/edit-form";
-import {fetchCustomers} from "@/app/lib/data";
+import {fetchInvoiceById, fetchCustomers} from "@/app/lib/data";
 
 export default async function Page({params}: { params: { id: string } }) {
     const id = params.id;
+    const [invoice, customers] = await Promise.all([
+        fetchInvoiceById(id),
+        fetchCustomers()
+    ]);
     return (
         <main>
             <Breadcrumbs breadcrumbs={[
